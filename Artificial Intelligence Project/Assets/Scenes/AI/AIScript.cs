a4b1c81441aaa6b0c46be15cfee5ef41b2f53c36
Kenneth Lee <kennethl123@nycstudents.net>
2024-01-14 18:50:05 -0500

    Added Ghost InstaKill (fixed collider)

--- Artificial Intelligence Project/Assets/Scenes/AI/AIScript.cs
+++ Artificial Intelligence Project/Assets/Scenes/AI/AIScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AIScript : MonoBehaviour
 {
     public GameObject Player;
     private Transform PlayerTransform;
     public float RotSpeed = 3.0f, MoveSpeed = 3.0f;
+
+    public AudioSource DeathSound;
 
     // Use this for initialization
     void Start()
@@ -22,6 +25,26 @@ public class AIScript : MonoBehaviour
 
         /* Move at Player*/
         transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            EndGame();
+        }
+    }
+
+    public void EndGame()
+    {
+        DeathSound.Play();
+
+        Destroy(gameObject);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
 
